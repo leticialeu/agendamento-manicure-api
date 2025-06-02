6632b6daa3ed460b336bd37c32be892d3760b583
leticia oliveira <83239872+antonycabral@users.noreply.github.com>
2025-06-02 17:51:24 -0300

    feat: inicializa projeto spring boot

---
+++ agendamento-manicure/src/main/java/com/example/agendamento_manicure/AgendamentoManicureApplication.java
@@ -0,0 +1,13 @@
+package com.example.agendamento_manicure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoManicureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoManicureApplication.class, args);
+	}
+
+}
---
+++ agendamento-manicure/src/test/java/com/example/agendamento_manicure/AgendamentoManicureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agendamento_manicure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoManicureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
